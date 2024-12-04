b25b314b9ba7f044181295fbf19f9b210a66a5f0
nanad-7 <nanad0019@gmail.com>
2024-12-05 00:14:32 +0100

    Add files via upload

---
+++ COS ASSIGNMENT.py
@@ -0,0 +1,15 @@
+dictionary = {
+    'Nnoo':'Hello',
+    'kedu':'How are you?',
+    'Imela':'Thank you',
+    'Dalu':'Goodbye',
+    'Ndo':'sorry',
+    'Kachifo':'Good night',
+    'ala aka':'ankle',
+    'akwara':'vein',
+    'afo':'stomach',
+    'bia':'come'
+}
+
+word = input('enter an igbo word: ')
+print(dictionary[word])
